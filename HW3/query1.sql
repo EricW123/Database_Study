b4913df47b01fd5d2eebebeef402d02afac3b19b
EricW123 <wangp13@rpi.edu>
2024-10-03 18:59:19 -0700

    hw3-q1

--- HW3/query1.sql
+++ HW3/query1.sql
@@ -1,6 +1,6 @@
 CREATE TABLE "MusicVideo" (
-	"video_id"	INTEGER,
+	"video_id"	INTEGER NOT NULL,
 	"track_id"	INTEGER UNIQUE,
-	"video_director"	TEXT,
+	"video_director"	TEXT NOT NULL,
 	PRIMARY KEY("video_id")
 );
